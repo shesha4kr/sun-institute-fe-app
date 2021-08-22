5639573af5951113f5cc0ec6f16003b6654b7b9d
shesha4kr <shesha4kr@gmail.com>
2021-08-22 20:40:47 +0530

    Generated Login Guard to guard LoginPage

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate() {
+    return true;
+  }
+}
