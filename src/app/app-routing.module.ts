d7b7165d4c59483bdf6573126315356d7e668cee
shesha4kr <shesha4kr@gmail.com>
2021-08-22 13:27:40 +0530

    Created Lazy Modules Student and Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
-const routes: Routes = [{ path: '', component: LoginPageComponent }];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'student', loadChildren: () => import('./modules/student/student.module').then(m => m.StudentModule) },
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
