99df5b97c04edfc414add3839fd801be19e3f988
shesha4kr <shesha4kr@gmail.com>
2021-08-27 20:42:27 +0530

    Using Setter instead of ngOnChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,11 +16,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     MatSlideToggleModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
